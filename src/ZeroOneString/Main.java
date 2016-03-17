23b56b1d263e17e0909193debc08551adac03330
Littlewhite <964873559@qq.com>
2016-03-17 16:43:55 +0800

    .

---
+++ src/ZeroOneString/Main.java
@@ -0,0 +1,12 @@
+package ZeroOneString;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<32;i++){
+			System.out.println(String.format("%5s", Integer.toString(i, 2)).replace(" ", "0"));
+		}
+
+	}
+
+}
